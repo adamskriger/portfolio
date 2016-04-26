bf93429717fc5631a82d9f1b8deac32eb98c8daa
github username <github email address>
2016-04-26 01:04:37 -0400

    Color Picking Has Begun

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -19,15 +19,15 @@ export default class Navigation extends React.Component {
         <span className="icon-bar"></span>
         <span className="icon-bar"></span>
       </button>
-      <a className="navbar-brand" href="#">Brand</a>
+      <a className="navbar-brand" href="#">Conscious</a>
     </div>
 
     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul className="nav navbar-nav">
-        <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li className="active"><a href="#">About Us <span className="sr-only">(current)</span></a></li>
+        <li><a href="#">Our Promise</a></li>
         <li className="dropdown">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Conditions We Treat <span className=""></span></a>
           <ul className="dropdown-menu">
             <li><a href="#">Action</a></li>
             <li><a href="#">Another action</a></li>
@@ -38,16 +38,17 @@ export default class Navigation extends React.Component {
             <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
+        <li><a href="#">Becoming a Patient</a></li>
+
       </ul>
 
       <ul className="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
         <li className="dropdown">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Get In Touch <span className=""></span></a>
           <ul className="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Phone</a></li>
+            <li><a href="#">Email</a></li>
+            <li><a href="#">In Person</a></li>
             <li role="separator" className="divider"></li>
             <li><a href="#">Separated link</a></li>
           </ul>
