2662664e2c8e55a245629972d3783c4de51f97af
github username <github email address>
2016-04-29 13:13:48 -0400

    Firebase is now hooked up

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 import App from './components/App.jsx'
 import React from 'react';
 import About from './components/About'
+import Blogger from './components/blogger'
 
 import {Router, Route, IndexRoute} from 'react-router';
 
@@ -11,6 +12,8 @@ export default (
 <Route path="/" component={App}>
 </Route>
 <Route path="/about" component={About} />
+<Route path="/blogger" component={Blogger} />
+
 </Router>
 
 );
