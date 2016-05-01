fbfed391df0c12aa8c256c91c07f60ea8a3c5973
github username <github email address>
2016-05-01 13:37:29 -0400

    About to deploy to heroku

--- app/actions/BlogActions.js
+++ app/actions/BlogActions.js
@@ -1,3 +1,3 @@
 import alt from '../libs/alt';
 
-export default alt.generateActions('read', 'mountFirebase', 'handleClick');
+export default alt.generateActions('read', 'mountFirebase', 'handleClick', 'filterList');
