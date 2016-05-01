42066301f23ab654d9761a2d6a9123791a9639b2
github username <github email address>
2016-05-01 16:51:08 -0400

    heroku

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: 'webpack --config ./webpack.prod.config.js --progress --colors'
 };
 
 process.env.BABEL_ENV = TARGET;
