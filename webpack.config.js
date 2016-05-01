7527eaac1355c6cc812e77822accc593c2532ee6
github username <github email address>
2016-05-01 17:57:03 -0400

    heroku many changes made to config file for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,25 @@ const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const pkg = require('./package.json');
 const TARGET = process.env.npm_lifecycle_event;
-
+const WebPackDevServer = require ('webpack-dev-server');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css')
 };
 
 process.env.BABEL_ENV = TARGET;
 
 const common = {
   entry: {
-    app: PATHS.app
+    app: PATHS.app, 
+    style: PATHS.style
   },
   // Add resolve.extensions
   // '' is needed to allow imports without an extension
@@ -24,17 +30,11 @@ const common = {
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: 'index.js'
   },
   module: {
     loaders: [
-      {
-        // Test expects a RegExp! Notethe slashes!
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        //Include accepts either a path or an array of paths
-        include: PATHS.app
-      },
+
       //set up JSX. This accepts js too thanks to RegExp
       {
       test: /\.(js|jsx)$/,
@@ -49,7 +49,15 @@ const common = {
       include: PATHS.app
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'blog app',
+      appMountId: 'app',
+      inject: false
+    })
+  ]
 };
 
 // Default configuration. We will return this if
@@ -58,7 +66,6 @@ if(TARGET === 'start' || !TARGET){
   module.exports = merge(common, {
     devtool: 'eval-source-map',
     devServer: {
-      contentBase: PATHS.build,
 
       //enable history API fallback so HTML5 HISTORY API based
       // routing works. This is a good default that will come in handy in more
@@ -77,6 +84,18 @@ if(TARGET === 'start' || !TARGET){
 
 },
 
+module:   {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
+
+
+
+    },
 plugins: [
   new webpack.HotModuleReplacementPlugin(),
   new NpmInstallPlugin({
@@ -87,5 +106,35 @@ plugins: [
 }
 
 if(TARGET === 'build') {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    entry: {
+      vendor: Object.keys(pkg.dependencies).filter(function(v){
+        return v !== 'alt-utils';
+      })
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+     module: {
+      loaders: [
+        // Extract CSS during build
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+} ]
+},
+    plugins: [
+      new CleanPlugin([PATHS.build], {
+
+      }),
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      })
+    ]
+  });
 }
