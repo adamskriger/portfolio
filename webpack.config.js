57b73c43db3257db11f809d16592135279264a9d
github username <github email address>
2016-05-01 19:21:30 -0400

    Webpacking

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,23 @@ const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
-const TARGET = process.env.npm_lifecycle_event;
 
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const pkg = require('./package.json');
+
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css'),
 };
+
+
 
 process.env.BABEL_ENV = TARGET;
 
@@ -86,35 +96,47 @@ plugins: [
 });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
-    devtool: 'eval-source-map',
-    devServer: {
-      contentBase: PATHS.build,
-
-      //enable history API fallback so HTML5 HISTORY API based
-      // routing works. This is a good default that will come in handy in more
-      // complicated setups.
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      progress: true,
-
-      //display only errors to reduce output amount
-      stats: 'errors only',
-
-      //Parse host and port from env so this is easy to customize
-      host: process.env.HOST,
-      port: process.env.PORT
-
-},
-
-plugins: [
-  new webpack.HotModuleReplacementPlugin(),
-  new NpmInstallPlugin({
-    save: true //--save
-  })
-]
-});
-});
+    entry: {
+      vendor: Object.keys(pkg.dependencies).filter(function(v) {
+        return v !== 'alt-utils';
+      }),
+      style: PATHS.style
+    },
+    output: {
+      path: PATHS.build,
+      // Output using entry name
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        // Extract CSS during build
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new CleanPlugin([PATHS.build]),
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      // Extract vendor and manifest files
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
+      // Setting DefinePlugin affects React library size!
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
 }
