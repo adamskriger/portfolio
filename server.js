1d194b645ae34e219e809d0141fafabec62eb3b6
github username <github email address>
2016-05-02 10:16:51 -0400

    added express

---
+++ server.js
@@ -0,0 +1,24 @@
+/* Global Requires */
+
+const express    = require('express');
+const logger     = require('morgan');
+const bodyParser = require('body-parser');
+const path       = require('path');
+const app        = express();
+
+if(process.env.NODE_ENV === 'development') {
+  console.log('in development.');
+  require('dotenv').config();
+} else {
+  console.log('in production.');
+}
+
+/* App Config */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'app')));
+app.use(logger('dev'));
+/* Server Initialization */
+app.get('/', (req, res) => res.sendFile('build/index.html'));
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server initialized on // ${new Date()}`));
