1b651ec0e2d60a8d5ae07a81c8d6539b0d8e0ab2
github username <github email address>
2016-05-01 22:25:44 -0400

    procfile and ports added

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,3 @@
-import './main.css';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes'
 
@@ -15,7 +14,7 @@ const logger = require('morgan');
 persist(alt, storage, 'app');
 var bs = require('bootstrap');
 import {Route, IndexRoute} from 'react-router';
-
+import WebPackDevServer from 'webpack-dev-server'
 
 ReactDOM.render(
   <Router history={browserHistory} routes={routes} >
