803a469a4d06efa2e1f0d0b5c6aec5ab1d3cd07c
github username <github email address>
2016-05-07 15:53:54 -0400

    Basic layout achieved

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,8 @@
 import AltContainer from 'alt-container';
 import React from 'react';
 import Website from './Website'
+import Heading from './Heading'
+
 
 //firebase
 import Firebase from 'firebase';
@@ -12,15 +14,21 @@ export default class App extends React.Component {
 
   render() {
 
+    var website = {
+      
+      fontSize: '1.5rem'
+    }
     return (
 <div className="outer">
-  <div className="inner"> 
-            <div><Website title={'Popstar'} url={'https://popstar-app.herokuapp.com/'} /></div>
-            <div><Website title={'Prolific Capital'} url={'http://prolific-capital.com/'} /></div>
-            <div><Website title={'ABC Apparel'} url={'https://vpv.herokuapp.com/'} /></div>
-            <div><Website title={'Real Time YouTube'} url={'https://youtube-react-spa.herokuapp.com/'} /></div>
-            <div><Website title={'Plantee App'} url={'45.55.179.238:3000'} /></div>
-            <div><Website title={'The Information Station'} url={'http://adamskriger.github.io/trivia/'} /></div>
+  <div className="inner">
+
+            <Heading />
+            <div style={website}><Website title={'Popstar'} url={'https://popstar-app.herokuapp.com/'} /></div>
+            <div style={website}><Website title={'Prolific Capital'} url={'http://prolific-capital.com/'} /></div>
+            <div style={website}><Website title={'ABC Apparel'} url={'https://vpv.herokuapp.com/'} /></div>
+            <div style={website}><Website title={'Real Time YouTube'} url={'https://youtube-react-spa.herokuapp.com/'} /></div>
+            <div style={website}><Website title={'Plantee App'} url={'45.55.179.238:3000'} /></div>
+            <div style={website}><Website title={'The Information Station'} url={'http://adamskriger.github.io/trivia/'} /></div>
   </div>
 </div>
     );
---
+++ app/components/Heading.jsx
@@ -0,0 +1,36 @@
+import AltContainer from 'alt-container';
+import React from 'react';
+import ReactBoostrap from 'react-bootstrap'
+
+//firebase
+import Firebase from 'firebase';
+import ReactFire from 'reactfire';
+
+
+export default class App extends React.Component {
+
+
+  render() {
+
+    var headline = {
+
+      fontSize: '4rem',
+      fontWeight: '18.7rem',
+      lineHeight: '3.3rem',
+      marginTop: '15rem',
+      marginBottom: '7.5rem'
+    }
+
+    return (
+      <div>
+
+      <div className="" style={headline}>Simply Advanced Web Development.</div>
+
+
+
+      </div>
+
+    );
+  }
+
+}
