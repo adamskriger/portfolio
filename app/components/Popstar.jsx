6c9e94eb639e2df20c012f3bc9e55108e4cb577f
github username <github email address>
2016-05-07 23:40:02 -0400

    Edited popstar headline

--- app/components/Popstar.jsx
+++ app/components/Popstar.jsx
@@ -20,13 +20,15 @@ export default class Popstar extends React.Component {
     const roleButtons = <button>Full Stack Development</button>
     const techButtons = <div><button>Ruby-on-Rails</button> <button>Amazon Web Services</button> <button>Secure User Authorization</button></div>
     const exhibitImage = <img className="twelve columns u-max-full-width" src="https://s3.amazonaws.com/akportfolioimages/Exhibit/popstar.png" alt=""/>
-    const projectHeadline = <div className= "row">Introducing Popstar</div>
+    const projectHeadline = <div className= "row"><a href="https://popstar-app.herokuapp.com/">Visit Site</a></div>
+
+
 
     const projectDetails = <div className= "row">Project Details</div>
     return (
 
       <div>
-      <Exhibit title={'Popstar'} type={'TYPE'} typeButtons={typeButtons} roleButtons={roleButtons} techButtons={techButtons} exhibitImage={exhibitImage} projectHeadline={projectHeadline} projectDetails={projectDetails} />
+      <Exhibit title={'Popstar—Airbnb for Commercial Real Estate'} type={'TYPE'} typeButtons={typeButtons} roleButtons={roleButtons} techButtons={techButtons} exhibitImage={exhibitImage} projectHeadline={projectHeadline} projectDetails={projectDetails} />
       </div>
 
     )
