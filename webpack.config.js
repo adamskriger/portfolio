a640aebc9cb00f400b9b1027dd0ecc88342947ce
github username <github email address>
2016-05-18 08:05:56 -0400

    Attempting to add my-logo.png

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,50 @@ const merge = require('webpack-merge');
 const webpack = require('webpack');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
 const TARGET = process.env.npm_lifecycle_event;
-
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var favicons = require('favicons'),
+    source = 'my-logo.png',           // Source image(s). `string`, `buffer` or array of `{ size: filepath }`
+    configuration = {
+        appName: null,                  // Your application's name. `string`
+        appDescription: null,           // Your application's description. `string`
+        developerName: null,            // Your (or your developer's) name. `string`
+        developerURL: null,             // Your (or your developer's) URL. `string`
+        background: "#fff",             // Background colour for flattened icons. `string`
+        path: "/",                      // Path for overriding default icons path. `string`
+        url: "/",                       // Absolute URL for OpenGraph image. `string`
+        display: "standalone",          // Android display: "browser" or "standalone". `string`
+        orientation: "portrait",        // Android orientation: "portrait" or "landscape". `string`
+        version: "1.0",                 // Your application's version number. `number`
+        logging: false,                 // Print logs to console? `boolean`
+        online: false,                  // Use RealFaviconGenerator to create favicons? `boolean`
+        icons: {
+            android: true,              // Create Android homescreen icon. `boolean`
+            appleIcon: true,            // Create Apple touch icons. `boolean`
+            appleStartup: true,         // Create Apple startup images. `boolean`
+            coast: true,                // Create Opera Coast icon. `boolean`
+            favicons: true,             // Create regular favicons. `boolean`
+            firefox: true,              // Create Firefox OS icons. `boolean`
+            opengraph: true,            // Create Facebook OpenGraph image. `boolean`
+            twitter: true,              // Create Twitter Summary Card image. `boolean`
+            windows: true,              // Create Windows 8 tile icons. `boolean`
+            yandex: true                // Create Yandex browser icon. `boolean`
+        }
+    },
+    callback = function (error, response) {
+        if (error) {
+            console.log(error.status);  // HTTP error code (e.g. `200`) or `null`
+            console.log(error.name);    // Error name e.g. "API Error"
+            console.log(error.message); // Error description e.g. "An unknown error has occurred"
+        }
+        console.log(response.images);   // Array of { name: string, contents: <buffer> }
+        console.log(response.files);    // Array of { name: string, contents: <string> }
+        console.log(response.html);     // Array of strings (html elements)
+    };
 
+favicons(source, configuration, callback);
 const pkg = require('./package.json');
 
 const PATHS = {
@@ -87,7 +126,9 @@ plugins: [
   new webpack.HotModuleReplacementPlugin(),
   new NpmInstallPlugin({
     save: true //--save
-  })
+  }),
+  new FaviconsWebpackPlugin('my-logo.png')
+
 ]
 });
 }
